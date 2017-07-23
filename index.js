5921dae6f0cbb8ac54c837005ccffd1e6e3b2079
Wojciech Bilicki <wojciech.bilicki@gmail.com>
2017-07-23 16:06:07 +0200

    step2-index.js-props

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+var topBarTitle = function(props) {
+  return React.createElement('p', { style: props.style }, props.title);
+};
+
 var topBar = function() {
   return React.createElement(
     'div',
     { style: { color: 'red' } },
-    React.createElement('hi', null, 'Marvel Superheroes')
+    React.createElement(topBarTitle, {
+      title: 'SpiderMan Hero',
+      style: { color: 'blue' }
+    }),
+    React.createElement(topBarTitle, { title: 'IronMan Hero' }),
+    React.createElement(topBarTitle, {
+      title: 'Hulk Hero',
+      style: { color: 'green' }
+    })
   );
 };
 ReactDOM.render(React.createElement(topBar), document.getElementById('app'));
