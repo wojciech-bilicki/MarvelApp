06c0bb1530f0f2dd5183d0823e0a773a57383612
wnadurski <w.nadurski@gmail.com>
2017-08-20 21:31:15 +0200

    Adding serving of static files

--- server/controllers/Heroes.js
+++ server/controllers/Heroes.js
@@ -1,15 +1,24 @@
 'use strict';
 const {getTable, getRecordBy, addRecord, removeRecordBy} = require("../db/db.js")
 var url = require('url');
-
-
+const port = require("../config.js").port
 var Heroes = require('./HeroesService');
 
-const getHeroBy = getRecordBy.bind(null, "heroes")
+
+const makeHeroImagePath = imageName => `http://localhost:${port}/assets/portraits/${imageName}`
+const withImage = hero => Object.assign({}, hero, {image: makeHeroImagePath(hero.image)})
+const getHeroByFromDb = selector => {
+  const hero = getRecordBy("heroes",selector)
+
+  if(!hero) return hero
+
+  return withImage(hero)
+}
+const getHeroesFromDb = () => getTable('heroes').map(withImage)
 
 module.exports.addFavourite = function addFavourite (req, res, next) {
   const heroId = req.swagger.params.heroId.value
-  const hero =  getHeroBy(hero => hero.id === heroId)
+  const hero =  getHeroByFromDb(hero => hero.id === heroId)
 
   if(!hero) {
     res.statusCode = 404
@@ -46,7 +55,7 @@ module.exports.deleteFavourite = function deleteFavourite (req, res, next) {
 
 module.exports.getFavourites = function getFavourites (req, res, next) {
   const favouritesIds = getTable('favourites')
-  const allHeros = getTable('heroes')
+  const allHeros = getHeroesFromDb()
 
   const favourites = allHeros.filter(hero => favouritesIds.includes(hero.id))
   res.setHeader('Content-Type', 'application/json')
@@ -57,7 +66,7 @@ module.exports.getFavourites = function getFavourites (req, res, next) {
 module.exports.getHeroById = function getHeroById (req, res, next) {
   const heroId = req.swagger.params.heroId.value
 
-  const hero =  getHeroBy(hero => hero.id === heroId)
+  const hero =  getHeroByFromDb(hero => hero.id === heroId)
 
   if(hero) {
     res.setHeader('Content-Type', 'application/json')
@@ -71,9 +80,12 @@ module.exports.getHeroById = function getHeroById (req, res, next) {
 };
 
 module.exports.getHeroes = function getHeroes (req, res, next) {
-  const heroes = getTable('heroes')
+  const heroes = getHeroesFromDb()
 
   res.setHeader('Content-Type', 'application/json')
   res.statusCode = 200
   res.end(JSON.stringify(heroes, null, 2));
 };
+
+module.exports.getHeroesFromDb = getHeroesFromDb
+module.exports.getHeroByFromDb = getHeroByFromDb
