b516fd467573597eadbea161fe926a83d9b0854a
wojciech-bilicki <wojciech.bilicki@gmail.com>
2017-10-05 18:35:20 +0200

    7th_session

--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
-'use strict';
 
-var app = require('connect')();
-var cors = require('cors')
-var http = require('http');
-var swaggerTools = require('swagger-tools');
-var jsyaml = require('js-yaml');
-var fs = require('fs');
-var serverPort = require('./config').port;
-var serveStatic = require('serve-static')
+
+let app = require('connect')();
+let cors = require('cors')
+let http = require('http');
+let swaggerTools = require('swagger-tools');
+let jsyaml = require('js-yaml');
+let fs = require('fs');
+let serverPort = require('./config').port;
+let serveStatic = require('serve-static')
 
 // swaggerRouter configuration
-var options = {
+let options = {
   swaggerUi: '/swagger.json',
   controllers: './controllers',
-  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+  useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
 };
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
+let spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
+let swaggerDoc = jsyaml.safeLoad(spec);
 
 // Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+swaggerTools.initializeMiddleware(swaggerDoc, (middleware) => {
   // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
   app.use(middleware.swaggerMetadata());
 
@@ -30,7 +30,7 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   // Validate Swagger requests
   app.use(middleware.swaggerValidator());
 
-  app.use(cors())
+  app.use(cors());
   // Route validated requests to appropriate controller
   app.use(middleware.swaggerRouter(options));
 
@@ -38,7 +38,7 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   app.use(middleware.swaggerUi());
 
   // Start the server
-  http.createServer(app).listen(serverPort, function () {
+  http.createServer(app).listen(serverPort, () => {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
   });
